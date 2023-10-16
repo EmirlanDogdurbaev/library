9613d88369afd404a0fb2c3ed32e8c2fc00288fd
Emirlan Dogdurbaev <jigit38@gmail.com>
2023-10-16 15:03:46 +0600

    added Serach Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
+import SearchBar from "./components/SearchBar/SearchBar";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
 
@@ -8,10 +9,13 @@ function App() {
   return (
     <div className="main">
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <div className="body">
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import cl from "./SearchBar.module.scss";
+
+import lupa from "../../assets/icons/Outline/Search/Magnifer.png";
+import user from "../../assets/icons/Outline/Search/userProf.png";
+const SearchBar = () => {
+  return (
+    <div className={cl.SearchBar}>
+      <div>
+        <span>
+          <input type="text" placeholder="поиск книг" />
+          <button>
+            <img src={lupa} alt="lupa image" />
+          </button>
+        </span>
+        <Link to={"/"}>
+          <img src={user} alt="profile button" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
