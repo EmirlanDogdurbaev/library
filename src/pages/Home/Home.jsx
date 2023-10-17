28120b3bd16b12538f2168f370be1dc114d982f2
Emirlan Dogdurbaev <jigit38@gmail.com>
2023-10-17 21:19:41 +0600

    added SearchBar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
+import cl from "./Home.module.scss";
 const Home = () => {
   return (
-    <div>
-      <h2>Home page</h2>
+    <div className={cl.Home}>
+      <h1>Welcome to our Web Site</h1>
     </div>
   );
 };
