66640cf0a28cdce59ac570b64ea550516bc4901b
Emirlan Dogdurbaev <jigit38@gmail.com>
2023-10-19 22:31:55 +0600

    added comments

--- src/components/OneBooksPage/OneBooksPage.jsx
+++ src/components/OneBooksPage/OneBooksPage.jsx
@@ -1,14 +1,19 @@
 import Book from "./Book/Book";
+import Comment from "./Comment/Comment";
 import cl from "./OneBooksPage.module.scss";
 const OneBookPage = () => {
   return (
     <div className={cl.OneBookPage}>
-      <Book />
-      <div>
+      <section>
+        <Book />
         <h3>Отзывы</h3>
-
-        
-      </div>
+        <div className={cl.comment_cont}>
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
+      </section>
     </div>
   );
 };
