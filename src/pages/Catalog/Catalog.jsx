149173d3fac154241603105f36d80f84b19d1df5
EmirlanDogdurbaev <jigit38@gmail.com>
2023-10-22 21:06:47 +0600

    create Catalog page

---
+++ src/pages/Catalog/Catalog.jsx
@@ -0,0 +1,11 @@
+import styles from "./Catalog.module.scss" 
+
+const Catalog = () => {
+  return (
+    <div className={styles.Catalog}>
+      <h1>Catalog page</h1>
+    </div>
+  );
+};
+
+export default Catalog;
